a6da1bcdd53fef76e31969a895806d135606ea0f
JulianRamirez2005 <solokiller2005@gmail.com>
2023-05-01 11:41:22 -0700

    sadasd
    sdasdsa

--- Unit5Ramirez/Assets/Scripts/GameManager.cs
+++ Unit5Ramirez/Assets/Scripts/GameManager.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Runtime.CompilerServices;
 
 public class GameManager : MonoBehaviour
 {
     private float spawnRate = 1.0f;
     public List<GameObject> targets;
+    private int score;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
     }
 
    
@@ -22,5 +28,7 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+      
+
     }
 }
--- Unit5Ramirez/Assets/Scripts/Target.cs
+++ Unit5Ramirez/Assets/Scripts/Target.cs
@@ -10,6 +10,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +32,16 @@ public class Target : MonoBehaviour
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
